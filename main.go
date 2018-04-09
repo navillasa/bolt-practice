0454a4e986f3347bf9d85cd2a206a0517f99aaff
Natalie Ventura Villasana <nat@storj.io>
2018-04-09 10:34:59 -0400

    changes logger to zap

--- main.go
+++ main.go
@@ -1,35 +1,30 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
-	"time"
 
 	"github.com/julienschmidt/httprouter"
 	"github.com/navillasa/bolt-practice/storage/boltdb"
-	"github.com/sirupsen/logrus"
-)
 
-var (
-	logger *logrus.Logger
+	"go.uber.org/zap"
 )
-
-func init() {
-	logger = logrus.StandardLogger()
-	logrus.SetLevel(logrus.DebugLevel)
-	logrus.SetFormatter(&logrus.TextFormatter{
-		ForceColors:     true,
-		FullTimestamp:   true,
-		TimestampFormat: time.RFC3339Nano,
-		DisableSorting:  true,
-	})
-}
 
 func main() {
-	logger.Info("Serving on localhost:3000")
+	logger, err := zap.NewDevelopment()
+	if err != nil {
+		fmt.Println("zap logger failed to instantiate")
+		return
+	}
+	defer logger.Sync()
+	logger.Info("serving on localhost:3000")
 
 	bdb, err := boltdb.New()
 	if err != nil {
-		logger.Fatalln(boltdb.ErrInitDb, err)
+		logger.Fatal("db error:",
+			zap.Error(boltdb.ErrInitDb),
+			zap.Error(err),
+		)
 		return
 	}
 
